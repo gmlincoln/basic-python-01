863cb3b133ee3e788425487eae58bc5467cc2fe1
Md Golam Maula <maula03@gmail.com>
2025-11-06 12:46:35 +0600

    class-13 added

---
+++ first.py
@@ -0,0 +1,68 @@
+print("Web Application Development with Python Batch-1")
+
+# 
+
+#Single Line Comment
+
+# print("Name: " + name)
+
+# print("Age: " +  age)
+
+
+# Multiple Line Comment
+
+""" 
+print(type(name))
+print(type(age))
+"""
+
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_married))
+
+
+# print("Name: " + name , type(name))
+
+# print(f"Name: {name}, he is {age} years old. He is {height}ft. He is {is_married}")
+
+
+# user_name = input("Enter your name: ")
+
+# print(f"Hello {user_name}!")
+
+# age = int(input("Enter your age: "))
+
+# print(f"{user_name} is {age} years old")
+
+# print(type(age))
+
+# num1 , num2 = map(int, input("Enter two numbers: ").split())
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 % num2)
+# print(num1 // num2)
+
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You're eligible for vote.")
+#     print("Okay")
+# else:
+#     print("You're not a voter!")
+
+# print("This is Bangladesh")
+
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 80:
+    print("A+")
+elif marks >= 70:
+    print("A")
+else:
+    print("F")
+
