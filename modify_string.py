865fe022b34ea3251edc087ee56d3090f6a15c4f
Md Golam Maula <maula03@gmail.com>
2026-01-03 17:59:05 +0600

    README.md file updated

---
+++ modify_string.py
@@ -0,0 +1,24 @@
+name = "lincoln"
+
+#Upper Case
+# print(name.upper())
+
+# another_name = "golam maula"
+# print(another_name.lower())
+
+
+# print(another_name.capitalize())
+# print(another_name.title())
+
+
+demo = "Sh,uvo,Ho,s,s,en"
+# print(demo)
+# print(demo.strip())
+
+# print(demo.replace("s", "GM"))
+
+# print(demo.split(","))
+
+sentence = "We are the so-called \"Vikings\" from the north."
+
+print(sentence)
